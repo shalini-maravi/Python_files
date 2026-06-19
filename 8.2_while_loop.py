2cd97fc4f0e9f4f3b785f25aa8ae63f91a2f4e04
shalini-maravi <sobhamaravi2006@gmail.com>
2026-06-19 19:55:04 +0530

    Add files via upload

---
+++ 8.2_while_loop.py
@@ -0,0 +1,103 @@
+## Example 1
+i = 1### W  H I L E - L O O P
+""" i=1
+while i<10:
+    print(i)
+    i=i+1
+  """
+'''
+## Factorial of---
+i=1
+fact=1
+no=int(input("Enter any number :")) 
+while i<=no:
+    fact=fact*i
+    i=i+1
+print("Factorial of ",no,"is",fact)
+ '''
+'''
+### While loop with else
+while i<=5:
+    if i==3:
+        break
+    print(i)
+    i = i+1
+else:
+    print("This is else part")
+'''
+'''
+## Example 2
+i = 1
+while i<=5:
+    print(i)
+    i = i+1
+else:
+    print("This is else part")
+'''
+'''
+## While loop with String
+str = "Easy"
+i=0
+while i<len(str):
+    print(str[i])
+    i=i+1
+'''
+'''
+## While loop with list
+girls = ["Ritu","Gitu","Situ"]
+i = 0
+while i<len(girls):
+    print(girls[i])
+    i=i+1
+'''
+'''
+## JUMP STATEMENT
+## Example 1
+for i in range(1,11):
+    print(i,end=" ")
+'''
+'''
+##  Example 2 same program with break
+for i in range(1,11):
+    if i ==5:
+        break
+    else:
+        print(i,end=" ")
+'''
+'''
+## Continue Statement
+## Example 1:
+for i in range(1,11):
+    if i==5:
+        continue
+    else:
+        print(i,end=" ")
+'''
+'''
+## Example 2:
+for i in range(1,11):
+    if i>=5:
+        continue
+    else:
+        print(i,end=" ")
+'''
+'''
+## Example 3:
+for i in range(1,11):
+    if i<=5:
+        continue
+    else:
+        print(i,end=" ")
+'''
+
+## Example 4:
+for i in range(1,11):
+    if i!=5:
+        continue
+    else:
+        print(i,end=" ")
+ 
+
+
+
+
