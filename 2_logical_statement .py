41c84a6df47611eb21e60526d834ccfc71027f8e
shalini-maravi <sobhamaravi2006@gmail.com>
2026-06-19 19:49:28 +0530

    Add files via upload

---
+++ 2_logical_statement .py
@@ -0,0 +1,33 @@
+## -- Logical statement (Only if)
+""" 
+    >> it is perform by condition.
+    >> if the codition is true then true side statement are executed.
+    >> only true side command work
+
+    syntax: - 
+        if exp:
+            statement..
+
+     Example: -
+        if 4>2:
+            print("True")
+
+ """
+## check the any number is perfect or not
+""" n = 4
+if n>0:
+    print('perfect') """
+
+# - Error code
+"""
+n = 'y'
+if n>0:
+    print('perfect')
+"""
+
+# check the match value
+'''value = 'shravani'
+if value=='shravani':
+    print("it's a perfect match")'''
+
+
