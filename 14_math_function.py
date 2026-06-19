fb4a54dff28ee22c9b3a0e7a9ca1f992562b454b
shalini-maravi <sobhamaravi2006@gmail.com>
2026-06-19 20:02:16 +0530

    Add files via upload

---
+++ 14_math_function.py
@@ -0,0 +1,61 @@
+# 1.sin()
+import math
+x=math.sin(2)
+print("Result:",x)
+
+# 2. cos()
+import math
+x=math.cos(2)
+print("Result:",x)
+
+# 3.tan()
+import math
+x=math.tan(2)
+print("Result:",x)
+
+# 4. sqrt()
+import math
+x=math.sqrt(16)
+print("Result:",x)
+y=math.sqrt(14)
+print("Result:",y)
+
+# 5.log10()
+import math
+x=math.log10(10)
+print("Result:",x)
+y=math.log10(5)
+print("Result:",y)
+
+# 6. pow()
+import math
+x=math.pow(2,3)
+print("Result:",x)
+y=math.pow(5,2)
+print("Result:",y)
+
+# 7.factorial()
+import math
+x=math.factorial(5)
+print("Factorial of 5:",x)
+y=math.factorial(4)
+print("Factorial of 4:",y)
+
+# 8. pow()
+import math
+x=math.exp(0)
+print("exp(0):",x)
+y=math.exp(4)
+print("exp(4):",y)
+
+# 9.floor()
+import math
+print(math.floor(3.2))
+print(math.floor(3.5))
+print(math.floor(3.9))
+
+# 10. ceil()
+import math
+print(math.ceil(3.2))
+print(math.ceil(3.5))
+print(math.ceil(3.9))
