492264c0e0ff95a1d4b78040d3200cb60bb7f4fe
shalini-maravi <sobhamaravi2006@gmail.com>
2026-06-19 19:48:53 +0530

    Add files via upload

---
+++ 1_basic.py
@@ -0,0 +1,36 @@
+#Basic print and parameter 'end'
+""" print('Hello world',end=' ') # bydefault - end='\n'
+print('python programming')
+"""
+#parameter 'sep'
+""" a=45
+print('A::',a,sep='=') # bydefault - sep= ' ' """
+
+##print multiple lines
+""" print(''' hello friend
+my first coding work
+python in a best compiler'''
+)"""
+
+##calculation area of circle
+pia = 3.14
+r=2
+aoc = pia * r * r
+print("Area of circle ::",aoc)
+
+""" 
+-----  EXTRA CLASS WORK -----
+#Area of Rectangle
+length=5
+width=3
+aor= length * width
+print("Area of rectangle ::",aor)
+print( )
+
+#Table Design
+print("serial\t Amount\t quantity\t")
+print("rice\t 30\t 20g")
+print("soap\t 40\t 4")
+print("pens\t 30\t 7")
+print("oats\t40\t10g") """
+
