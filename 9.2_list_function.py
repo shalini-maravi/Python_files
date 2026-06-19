4fbd8631483797221a4d2be90c15128002470b88
shalini-maravi <sobhamaravi2006@gmail.com>
2026-06-19 19:57:01 +0530

    Add files via upload

---
+++ 9.2_list_function.py
@@ -0,0 +1,29 @@
+#1. len()
+""" fruit_list=["Apple","Orange","Mango"]
+print ("List elements : ", fruit_list) 
+print("Length of list is ",len(fruit_list)) """
+#2. Max()
+""" fruit_list=["Apple","Orange","Mango"]
+print("Fruits list  :",fruit_list) 
+print ("Max elements : ", max(fruit_list))
+animal_list=["Zebra","Dog","Elephant"] 
+print("Animal list  :",animal_list) 
+print ("Max elements : ", max(animal_list))
+int_list=[45,85,36]
+print("int list     :",int_list)
+print ("Max elements : ", max(int_list)) """
+#3. min()
+""" fruit_list=["Apple","Orange","Mango"] 
+print("Fruits list :",fruit_list) 
+print ("Min elements : ", min(fruit_list)) 
+animal_list=["Zebra","Dog","Elephant"] 
+print("Animal list :",animal_list) 
+print ("Min elements : ", min(animal_list))
+int_list=[45,85,36] 
+print("int list :",int_list)
+print ("Min elements : ", min(int_list)) """
+#4.List
+fruit_tuple=("Apple","Orange","Mango") 
+print("Tuple Items:",fruit_tuple) 
+fruit_list=list(fruit_tuple)
+print("List Items :",fruit_list)
